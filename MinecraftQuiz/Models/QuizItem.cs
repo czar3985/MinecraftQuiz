065b59abadd7ca7950eb8996ffaa62060af2f317
Rina Lingad <pixie.czar@gmail.com>
2018-11-09 23:30:47 +1300

    Add validation

--- MinecraftQuiz/Models/QuizItem.cs
+++ MinecraftQuiz/Models/QuizItem.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -8,7 +9,13 @@ namespace MinecraftQuiz.Models
     public class QuizItem
     {
         public int ID { get; set; }
+
+        [StringLength(250, MinimumLength = 3)]
+        [Required]
         public string Question { get; set; }
+
+        [StringLength(60)]
+        [Required]
         public string Answer { get; set; }
     }
 }
