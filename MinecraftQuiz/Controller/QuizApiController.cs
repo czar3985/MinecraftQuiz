f3810119bf57c1d97e14761b1145433612fb51fb
Rina Lingad <pixie.czar@gmail.com>
2018-11-11 17:26:13 +1300

    Add API implementation for GetAll items

---
+++ MinecraftQuiz/Controller/QuizApiController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using MinecraftQuiz.Models;
+
+namespace MinecraftQuiz.Controller
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class QuizApiController : ControllerBase
+    {
+        private readonly MinecraftQuiz.Models.MinecraftQuizContext _context;
+
+        public QuizApiController(MinecraftQuizContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/QuizApi
+        [HttpGet]
+        public ActionResult<List<QuizItem>> GetAll()
+        {
+            return _context.QuizItems.ToList();
+        }
+    }
+}
